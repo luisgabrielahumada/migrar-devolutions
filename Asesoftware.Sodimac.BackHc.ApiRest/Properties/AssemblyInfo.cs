464777ba0fcebee26db634ada60e496df8a004b3
Luis Gabriel Ahumada <luisgabrielahumada@gmail.com>
2026-06-25 18:10:21 -0400

    update

--- Asesoftware.Sodimac.BackHc.ApiRest/Properties/AssemblyInfo.cs
+++ Asesoftware.Sodimac.BackHc.ApiRest/Properties/AssemblyInfo.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Diagnostics;
-using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿using System.Reflection;
 using System.Runtime.InteropServices;
-using System.Runtime.Versioning;
-using Asesoftware.Sodimac.BackHc.ApiRest;
-using WebActivatorEx;
 
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: PreApplicationStartMethod(typeof(SwaggerConfig), "Register")]
+//[assembly: PreApplicationStartMethod(typeof(SwaggerConfig), "Register")]
 [assembly: AssemblyTitle("Asesoftware.Sodimac.BackHc.ApiRest")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
